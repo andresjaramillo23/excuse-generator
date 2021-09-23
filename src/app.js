ce50d5ad2eeee076142343f40a4ae14484dd630f
Andres Jaramillo <a.jaramillo24@gmail.com>
2021-09-23 00:21:26 +0000

    Added script and basic functions

--- src/app.js
+++ src/app.js
@@ -6,6 +6,33 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  console.log("Hello Rigo");
 };
+
+function getRandomInt(arraySize) {
+  return Math.floor(Math.random * (arraySize - 0) + 0);
+}
+
+function nonsenseGenerator() {
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  var nonsenseSentence =
+    who[getRandomInt(who.length)] +
+    " " +
+    action[getRandomInt(action.length)] +
+    " " +
+    what[getRandomInt(what.length)] +
+    " " +
+    when[getRandomInt(when.length)];
+
+  return nonsenseSentence;
+}
