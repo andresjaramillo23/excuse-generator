56d08fbd87f80f4fcec3b01bb3508650027b8351
Andres Jaramillo <a.jaramillo24@gmail.com>
2021-09-24 13:15:53 +0000

    Added input text function

--- src/app.js
+++ src/app.js
@@ -9,36 +9,41 @@ window.onload = function() {
   console.log("Hello Rigo");
 };
 
+let who = ["The dog", "My grandma", "His turtle", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "the keys", "the car"];
+let when = [
+  "before the class",
+  "right on time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying"
+];
+
 function getRandomInt(arraySize) {
   return Math.floor(Math.random() * arraySize);
 }
 
 function nonsenseGenerator() {
-  let who = ["The dog", "My grandma", "His turtle", "My bird"];
-  let action = ["ate", "peed", "crushed", "broke"];
-  let what = ["my homework", "the keys", "the car"];
-  let when = [
-    "before the class",
-    "right on time",
-    "when I finished",
-    "during my lunch",
-    "while I was praying"
-  ];
+  return (
+    getSentence(who) +
+    getSentence(action) +
+    getSentence(what) +
+    getSentence(when)
+  );
+}
 
-  var nonsenseSentence =
-    who[getRandomInt(action.length)] +
-    " " +
-    action[getRandomInt(action.length)] +
-    " " +
-    what[getRandomInt(what.length)] +
-    " " +
-    when[getRandomInt(when.length)];
-
-  return nonsenseSentence;
+function getSentence(array) {
+  return array[getRandomInt(array.length)] + " ";
 }
 
 const btn = document.getElementById("b1");
 
 btn.onclick = function() {
+  var whoToBlame = document.getElementById("blameText").value;
+  if (whoToBlame.length > 0) {
+    who.push(whoToBlame);
+  }
+  document.getElementById("blameText").value = "";
   document.getElementById("excuse").innerHTML = nonsenseGenerator();
 };
